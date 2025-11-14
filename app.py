6535c4e04032e921d585f034c762c3a249c02359
zerr32 <ludrastar@gmail.com>
2025-11-14 20:20:23 +0700

    update kode app

--- app.py
+++ app.py
@@ -1,16 +1,18 @@
 
 import random 
 from datetime import datetime
+import pandas as pd
 
 ruangan = ["room1", "room2", "room3", "room4", "room5", "room6", "room7", "room8", "room9", "room10"]
 
+
 makan = {
-    "1": ("🍜 Mie Indomie", 5000),
-    "2": ("🍚 Nasi Goreng", 10000),
-    "3": ("🥚 Nasi Telur", 10000),
-    "4": ("🍳 Omurice", 15000),
-    "5": ("🍗 Nasi Ayam", 20000),
-    "6": ("🔥 Nasi Ayam Geprek", 25000)
+    "1": {"nama":"🍜 Mie Indomie", "harga": 5000},
+    "2": {"nama":"🍚 Nasi Goreng","harga":10000},
+    "3": {"nama": "🥚 Nasi Telur", "harga": 10000},
+    "4": {"nama":"🍳 Omurice", "harga":15000},
+    "5": {"nama":"🍗 Nasi Ayam","harga":20000},
+    "6": {"nama":"🔥 Nasi Ayam Geprek","harga":25000}
 }
 
 minum = {
@@ -77,14 +79,15 @@ for i in range(pelanggan):
 
     if pesan_makan.lower() == 'y':
         while True:
-            for kode, (nama_makanan, harga_makanan) in makan.items():
-                print(f"{kode}. {nama_makanan:<25} Rp{harga_makanan}")
+            for kode, item in makan.items():
+                print(f"{kode}. {item['nama']:<25} Rp{item['harga']}")
             pilih_makanan = input("Pilih makanan (1-6, 0 jika selesai): ")
 
             if pilih_makanan == '0':
                 break
             if pilih_makanan in makan:
-                nama_makanan, harga_makanan = makan[pilih_makanan]
+                nama_makanan = makan[pilih_makanan]['nama']
+                harga_makanan = makan[pilih_makanan]['harga']
                 porsi = int(input("Jumlah porsi: "))
                 subtotal = harga_makanan * porsi
                 pesanan_makanan.append((nama_makanan, porsi, subtotal))
@@ -95,7 +98,7 @@ for i in range(pelanggan):
     print()
 
     print("\n╔══════════════════════════════════╗")
-    print("║          🥤 MENU MINUMAN          ║")
+    print("║          🥤 MENU MINUMAN         ║")
     print("╚══════════════════════════════════╝")
 
     pesanan_minuman = []
@@ -123,14 +126,22 @@ for i in range(pelanggan):
     print()
     print("================================================================================================")
     print()
-    member = input("Apakah pelanggan adalah member? (y/n): ")
-    diskon = 0.2 if member.lower() == "y" else 0
-    potongan = total_semua * diskon
-    total_bayar = total_semua - potongan
 
-    print()
-    print("================================================================================================")
-    print()
+    
+    diskon = 0
+    diskon_vip = 0
+
+    if paket == "💎 Paket VIP":
+        diskon_vip = 0.1
+        potongan_vip = total_semua * diskon_vip
+        total_bayar = total_semua - potongan_vip
+    else:
+        member = input("Apakah anda member (y/n): ")
+        diskon = 0.2 if member.lower() == "y" else 0
+        potongan = total_semua * diskon
+        total_bayar = total_semua - potongan
+
+
     print("\n💳 PILIH METODE PEMBAYARAN 💳")
     print("1. Tunai")
     print("2. E-Wallet")
@@ -144,7 +155,10 @@ for i in range(pelanggan):
     if metode in ["1", "2", "3"]:
         while True:
             try:
-                bayar_uang = int(input(f"Total pembayaran: Rp{int(total_bayar)}\nMasukkan jumlah uang: "))
+                if paket == "💎 Paket VIP":
+                    bayar_uang = int(input(f"Anda mendapatkan potongan VIP {int(potongan_vip)}\nTotal pembayaran: Rp{int(total_bayar)}\nMasukkan jumlah uang: "))
+                else:
+                    bayar_uang = int(input(f"Anda mendapatkan potongan member {int(potongan)}\nTotal pembayaran: Rp{int(total_bayar)}\nMasukkan jumlah uang: "))
                 if bayar_uang < total_bayar:
                     print("⚠️ Uang kurang, silakan tambah lagi.")
                 else:
@@ -194,6 +208,8 @@ for i in range(pelanggan):
     print(f"Subtotal       : Rp{total_semua}")
     if diskon > 0:
         print(f"Diskon Member  : -Rp{int(potongan)}")
+    elif diskon_vip > 0:
+        print(f"Potongan member vip : -Rp{int(potongan_vip)}")
     print(f"TOTAL BAYAR    : Rp{int(total_bayar)}")
     print("------------------------------------")
     if metode in ["1", "2", "3"]:
@@ -204,4 +220,3 @@ for i in range(pelanggan):
     print("====================================")
     print("Mata modote kudasai~ ❤️  Arigatou gozaimasu goshujin-sama!")
     print("====================================\n")
-
